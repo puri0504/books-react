906bdd0313deacdbf37b71ca8d85652d57eeacb4
ksenia <puri1507@gmail.com>
2018-09-21 14:46:41 +0300

    add reducer and initial state

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,19 +1,29 @@
+import {COMPLETE_TODO} from '../consts/ActionTypes'
+
 const initialState = [
     {
-        id: 0,
+        id: 1,
+        text: 'Watch screencast',
+        desc: 'Watch screencast and make redux app',
+        completed: true,
+    },
+    {
+        id: 2,
         text: 'Use Redux',
-        desc: 'Use Redux desc',
+        desc: 'Add redux to your app',
+        completed: false,
+    },
+    {
+        id: 3,
+        text: 'Use router',
+        desc: 'Add router to your app',
         completed: false,
     }
 ]
 
 export default (state, action) => {
     switch (action.type) {
-        case 'COMPLETE_TODO':
-            return state.map(task => {
-                if (task.id === action.id) {
-
-                }
-            })
+        case COMPLETE_TODO:
+            return state.map(task => task.id === action.id ? {...task, completed: true} : task)
     }
 }
