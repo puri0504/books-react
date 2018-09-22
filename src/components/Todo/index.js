ae66dacdb34eca80affa609b2da3eee281d6a46e
ksenia <puri1507@gmail.com>
2018-09-22 21:46:57 +0300

    refactored completed

--- src/components/Todo/index.js
+++ src/components/Todo/index.js
@@ -5,14 +5,14 @@ import './style.css'
 class Todo extends Component {
     render() {
         const desc = this.props.isOpen && <div>{this.props.task.desc}</div>
+        const completed = this.props.task.completed ? <div className="completed">completed</div> : <button onClick={this.props.onComplete}>completed</button>
 
         return (
             <div className="Todo">
                 <h3>{this.props.task.title}</h3>
                 {desc}
-                <div className="completed">{this.props.task.completed ? 'completed' : ''}</div>
                 <button onClick={this.props.onToggleOpen}>{this.props.isOpen ? 'close' : 'show'}</button>
-                <button onClick={this.props.onComplete}>completed</button>
+                {completed}
             </div>
         )
     }
